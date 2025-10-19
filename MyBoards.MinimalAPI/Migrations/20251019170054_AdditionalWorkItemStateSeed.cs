6ccc31f25e1c48b7747b8b3b527d27e7d0468982
Grzegorz Kniażuk <grzegorz.kniazuk@gmail.com>
2025-10-19 19:04:30 +0200

    AdditionalWorkItemStateSeed migration

---
+++ MyBoards.MinimalAPI/Migrations/20251019170054_AdditionalWorkItemStateSeed.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBoards.MinimalAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdditionalWorkItemStateSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) {
+            migrationBuilder.InsertData(
+                table: "WorkItemStates",
+                column: "Value",
+                value: "On Hold"
+            );
+            
+            migrationBuilder.InsertData(
+                table: "WorkItemStates",
+                column: "Value",
+                value: "Rejected"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "WorkItemStates",
+                keyColumn: "Value",
+                keyValue: "On Hold"
+            );
+            
+            migrationBuilder.DeleteData(
+                table: "WorkItemStates",
+                keyColumn: "Value",
+                keyValue: "Rejected"
+            );
+        }
+    }
+}
