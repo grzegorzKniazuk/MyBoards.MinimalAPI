a529ca369bfc56c9faf7546238d4bd80b4d4882c
Grzegorz Kniażuk <grzegorz.kniazuk@gmail.com>
2025-10-19 19:32:31 +0200

    TagSeed

---
+++ MyBoards.MinimalAPI/Migrations/20251019171401_TagSeed.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBoards.MinimalAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class TagSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) {
+            migrationBuilder.InsertData(
+                table: "WorkItemTags",
+                column: "Value",
+                value: "Web"
+            );
+            
+            migrationBuilder.InsertData(
+                table: "WorkItemTags",
+                column: "Value",
+                value: "UI"
+            );
+            
+            migrationBuilder.InsertData(
+                table: "WorkItemTags",
+                column: "Value",
+                value: "Desktop"
+            );
+            
+            migrationBuilder.InsertData(
+                table: "WorkItemTags",
+                column: "Value",
+                value: "API"
+            );
+            
+            migrationBuilder.InsertData(
+                table: "WorkItemTags",
+                column: "Value",
+                value: "Service"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "WorkItemTags",
+                keyColumn: "Value",
+                keyValue: "Web"
+            );
+            
+            migrationBuilder.DeleteData(
+                table: "WorkItemTags",
+                keyColumn: "Value",
+                keyValue: "UI"
+            );
+            
+            migrationBuilder.DeleteData(
+                table: "WorkItemTags",
+                keyColumn: "Value",
+                keyValue: "Desktop"
+            );
+            
+            migrationBuilder.DeleteData(
+                table: "WorkItemTags",
+                keyColumn: "Value",
+                keyValue: "API"
+            );
+            
+            migrationBuilder.DeleteData(
+                table: "WorkItemTags",
+                keyColumn: "Value",
+                keyValue: "Service"
+            );
+        }
+    }
+}
